642af1768f3baae988dd981a3261dfd87c5684c9
George Salu <george.salu10@gmail.com>
2021-01-07 16:25:20 -0300

    formatando

--- src/main/java/com/library/api/resource/BookController.java
+++ src/main/java/com/library/api/resource/BookController.java
@@ -19,7 +19,6 @@ public class BookController {
 	private BookService service;
 	private ModelMapper modelMapper;
 	
-	
 	public BookController(BookService service, ModelMapper modelMapper) {
 		this.service = service;
 		this.modelMapper = modelMapper;
@@ -29,9 +28,7 @@ public class BookController {
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public BookDTO create(@RequestBody BookDTO dto) {
 		Book entity = modelMapper.map(dto, Book.class);
-		
 		entity = service.save(entity);
-		
 		return modelMapper.map(entity, BookDTO.class);
 	}
 	
