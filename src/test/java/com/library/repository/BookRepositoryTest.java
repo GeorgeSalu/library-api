510bf5f60f69c58ee04a827a6be7fb3ff9567736
George Salu <george.salu10@gmail.com>
2021-01-08 11:35:14 -0300

    removendo imports não utilizados

--- src/test/java/com/library/repository/BookRepositoryTest.java
+++ src/test/java/com/library/repository/BookRepositoryTest.java
@@ -1,9 +1,7 @@
 package com.library.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThat;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
