4e76ddb5d2fee10e846aa2adfe89a71f8d0e4cdc
George Salu <george.salu10@gmail.com>
2021-01-08 14:44:31 -0300

    implementando o cenario onde o livro não existe no servidor

--- src/test/java/com/library/api/resource/BookControllerTest.java
+++ src/test/java/com/library/api/resource/BookControllerTest.java
@@ -145,6 +145,21 @@ public class BookControllerTest {
 			.andExpect(jsonPath("author").value(book.getAuthor()))
 			.andExpect(jsonPath("isbn").value(book.getIsbn()));
 	}
+	
+	@Test
+	@DisplayName("Deve retornar resource not found quando o livro procurado não existir.")
+	public void bookNotFoundTest() throws Exception {
+
+		BDDMockito.given(service.getById(Mockito.anyLong())).willReturn(Optional.empty());
+		
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+				.get(BOOK_API.concat("/"+1))
+				.accept(MediaType.APPLICATION_JSON);
+
+		mvc
+			.perform(request)
+			.andExpect(status().isNotFound());
+	}
 
 
 	private BookDTO createNewBook() {
