d01a38bd1fc88182acf380a425d773568b408322
George Salu <george.salu10@gmail.com>
2021-01-08 10:54:50 -0300

    implementando servialVersionId no BusinessException

--- src/main/java/com/library/exception/BusinessExcetion.java
+++ src/main/java/com/library/exception/BusinessExcetion.java
@@ -2,6 +2,8 @@ package com.library.exception;
 
 public class BusinessExcetion extends RuntimeException{
 
+	private static final long serialVersionUID = 6827405651945003400L;
+
 	public BusinessExcetion(String s) {
 		super(s);
 	}
