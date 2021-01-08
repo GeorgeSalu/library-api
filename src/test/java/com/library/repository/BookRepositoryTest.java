364f99ec9976a5118953a63c764c4394f73cba8b
George Salu <george.salu10@gmail.com>
2021-01-08 16:27:51 -0300

    teste de integracao do metodo findById

--- src/test/java/com/library/repository/BookRepositoryTest.java
+++ src/test/java/com/library/repository/BookRepositoryTest.java
@@ -2,6 +2,9 @@ package com.library.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -51,6 +54,20 @@ public class BookRepositoryTest {
 		
 		//verificacao
 		assertThat(exists).isFalse();
+	}
+	
+	@Test
+	@DisplayName("Deve criar um livro por id.")
+	public void findByIdTest() {
+		//cenario
+		Book book = createValidBook("123");
+		entityManager.persist(book);
+		
+		//execucao
+		Optional<Book> foundBook = repository.findById(book.getId());
+		
+		//verificacao
+		assertThat(foundBook.isPresent()).isTrue();
 	}
 	
 	private Book createValidBook(String isbn) {
