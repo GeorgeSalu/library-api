bbd10f5abb136e95da97e8bc692d60a29b42f695
George Salu <george.salu10@gmail.com>
2021-01-09 14:20:04 -0300

    criando o design da consulta de livros por parametros

--- src/main/java/com/library/api/service/BookService.java
+++ src/main/java/com/library/api/service/BookService.java
@@ -1,6 +1,9 @@
 package com.library.api.service;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
+
+import org.springframework.data.domain.Page;
 
 import com.library.model.entity.Book;
 
@@ -13,5 +16,7 @@ public interface BookService {
 	void delete(Book book);
 
 	Book update(Book book);
+
+	Page<Book> find(Book filter, Pageable pageRequest);
 
 }
--- src/main/java/com/library/api/service/impl/BookServiceImpl.java
+++ src/main/java/com/library/api/service/impl/BookServiceImpl.java
@@ -1,7 +1,9 @@
 package com.library.api.service.impl;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import com.library.api.service.BookService;
@@ -45,6 +47,12 @@ public class BookServiceImpl implements BookService {
 			throw new IllegalArgumentException("Book id cant be null");
 		}
 		return this.repository.save(book);
+	}
+
+	@Override
+	public Page<Book> find(Book filter, Pageable pageRequest) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
