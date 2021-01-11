0cc28080865f6a93f2a9a8be77e4059c67a1f760
George Salu <george.salu10@gmail.com>
2021-01-11 12:18:36 -0300

    removendo import não utilizado

--- src/test/java/com/library/api/resource/LoanControllerTest.java
+++ src/test/java/com/library/api/resource/LoanControllerTest.java
@@ -1,6 +1,5 @@
 package com.library.api.resource;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
