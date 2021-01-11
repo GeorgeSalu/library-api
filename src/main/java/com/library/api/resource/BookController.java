731345f032831a577a6ab9438528da24d90eae1e
George Salu <george.salu10@gmail.com>
2021-01-11 15:35:58 -0300

    removendo imports não utilizados

--- src/main/java/com/library/api/resource/BookController.java
+++ src/main/java/com/library/api/resource/BookController.java
@@ -10,10 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,9 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.library.api.dto.BookDTO;
-import com.library.api.errors.ApiErrors;
 import com.library.api.service.BookService;
-import com.library.exception.BusinessException;
 import com.library.model.entity.Book;
 
 @RestController
