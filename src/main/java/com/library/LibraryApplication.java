22d006cc2cdec038cab804e990e3a50b7f309e19
George Salu <george.salu10@gmail.com>
2021-01-13 09:40:00 -0300

    testando o envio de emails

--- src/main/java/com/library/LibraryApplication.java
+++ src/main/java/com/library/LibraryApplication.java
@@ -1,18 +1,37 @@
 package com.library;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.library.api.service.EmailService;
+
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApplication {
+	
+	@Autowired
+	private EmailService emailService;
 
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+	
+	@Bean
+	public CommandLineRunner runner() {
+		return args -> {
+			List<String> emails = Arrays.asList("9c454516d6-05d2e2@inbox.mailtrap.io");
+			emailService.sendEmails("testando servico de emails", emails);
+			System.out.println("emails envidos");
+		};
 	}
 	
 	public static void main(String[] args) {
