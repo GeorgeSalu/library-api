90252c30bfc599aba1cd9a5facd8eecf54c8f926
George Salu <george.salu10@gmail.com>
2021-01-13 09:20:33 -0300

    configurando o servidor MailTrip para envio de email

---
+++ src/main/java/com/library/api/service/impl/EmailServiceImpl.java
@@ -0,0 +1,15 @@
+package com.library.api.service.impl;
+
+import java.util.List;
+
+import com.library.api.service.EmailService;
+
+public class EmailServiceImpl implements EmailService {
+
+	@Override
+	public void sendEmails(String mensagem, List<String> mailsList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
